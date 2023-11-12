ff82c878177184ed1cdc8ab4e1f31053efb359ca
JosephGlz <joseph220199@gmail.com>
2023-11-11 19:44:48 -0600

    feat: Refactor note list layout and add form dialog component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, Directive } from '@angular/core';
+import { Note } from './interfaces/Note';
+import { NoteService } from './services/note.service';
+
+import { MatDialog } from '@angular/material/dialog';
+import { FormDialogComponent } from './components/form-dialog/form-dialog.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
+
 export class AppComponent {
-  title = 'CursoAngularUAT';
+  selectedID: string = '';
+  constructor(
+    private noteService: NoteService,
+    public dialogRef: MatDialog
+  ) {}
+
+  get noteList(): Note[] {
+    return this.noteService.getNotes();
+  }
+
+  changeSelectedID(uuid: string): void {
+    this.selectedID = uuid;
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialogRef.open(FormDialogComponent, {
+      width: '700px',
+      data: {
+        uuid: this.selectedID
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { HttpClientModule } from '@angular/common/http';
+import { MatDialogModule } from '@angular/material/dialog';
 
 import { AppComponent } from './app.component';
+import { NoteCardComponent } from './components/note-card/note-card.component';
+import { ColorSelectorComponent } from './components/color-selector/color-selector.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormDialogComponent } from './components/form-dialog/form-dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoteCardComponent,
+    ColorSelectorComponent,
+    FormDialogComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    FormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
